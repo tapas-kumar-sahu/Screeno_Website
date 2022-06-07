2b581638c06dcdc77c853a81a5d728c87c096df5
Tapas Kumar Sahu <tapassahu04@gmail.com>
2022-06-07 16:03:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector("#toggle");
+const mobileMenus = document.querySelector(".mobile-menus");
+
+toggle.addEventListener("click",()=>{
+    mobileMenus.classList.toggle("hidden");
+})
+
+// --------------
+
+
+
+
